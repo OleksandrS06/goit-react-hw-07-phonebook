099352721e51a59f37492644ff0a06ae2181a756
OleksandrS06 <112249299+OleksandrS06@users.noreply.github.com>
2023-01-26 00:48:15 +0100

    for check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Form from './ContactAddForm/ContactAddForm';
+import ContactsList from './ContactsList/ContactsList';
+import Filter from './Filter';
+import {
+  getContactsValue,
+  getErrorValue,
+  getIsLoadingValue,
+} from 'redux/contactsSlice/contactsSlice';
+import { getContactsThunk } from 'redux/operations/contactThunk';
+import Loader from './Loader/Loader';
+
+const App = () => {
+  let contacts = useSelector(getContactsValue);
+  let isLoading = useSelector(getIsLoadingValue);
+  let error = useSelector(getErrorValue);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContactsThunk());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="wrapper">
+      <h1 className="heading">Phonebook</h1>
+      <Form />
+      {contacts.length > 0 ? (
+        <>
+          <h2>Find Contacts</h2>
+          <Filter />
+          <ContactsList />
+        </>
+      ) : (
+        <p>There are no any contacts at the moment</p>
+      )}
+      {isLoading && <Loader />}
+      {error && <h2>{error}</h2>}
     </div>
   );
 };
+
+export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { filterReducer } from './filterSlice/filterSlice';
+import { contactsReducer } from './contactsSlice/contactsSlice';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+});
